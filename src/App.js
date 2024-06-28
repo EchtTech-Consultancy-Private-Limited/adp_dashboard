6957dc7a5b3530f754ca2e3f006312f4ff26a342
VivekEchttech <vivek.kumar@echttech.com>
2024-06-28 20:47:50 +0530

    setup project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { HashRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+import { routes } from "./routes/index";
 function App() {
   return (
     <div className="App">
-  
+  <HashRouter>
+      <Header/>
+      {routes}
+    </HashRouter>
     </div>
   );
 }
