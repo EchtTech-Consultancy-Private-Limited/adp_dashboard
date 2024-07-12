c775ea3c12df5a4a902adad7243deeba31c89376
Gireesh <gireesh.shukla@echttech.com>
2024-07-12 10:32:07 +0530

    pre changes

---
+++ src/components/Home/Banners.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../Header/Header";
+export default function Banners() {
+  return (
+    <section className="bg-home">
+      <div className="container">
+        <Header />
+        <div className="row mt-5">
+          <div className="col-md-6">
+            hom,e bnanmnef
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
