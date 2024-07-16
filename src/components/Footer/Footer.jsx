947b320a87bc12ce6d0d8370fc9057e58c4a7f1f
Gireesh <gireesh.shukla@echttech.com>
2024-07-16 14:22:13 +0530

    pre changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import './Footer.scss'
+import './footer.scss'
 import { useTranslation } from "react-i18next";
 import AOS from "aos";
 import "aos/dist/aos.css";
