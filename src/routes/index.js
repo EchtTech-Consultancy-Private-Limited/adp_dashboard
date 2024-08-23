09eafd503618660b9f56a6515e3decda99dc8a35
VivekEchttech <159779804+VivekEchttech@users.noreply.github.com>
2024-08-23 16:35:35 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,11 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 import TransitionRateReport from '../components/Report/TransitionRateReport';
 import TeacherAndSchResourcesReport from '../components/Report/TeacherAndSchResourcesReport';
-import StudentsPerformanceReport from '../components/Report/StudentsPerformanceReport';
+
 import SchoolInfraStructureReport from '../components/Report/SchoolInfraStructureReport';
 import EnrollmentAndRetentionReport from '../components/Report/EnrollmentAndRetentionReport';
 import ScreenReader from '../pages/ScreenReader';
+import TeacherTrainedCwsnReport from '../components/Report/TeacherTrainedCwsnReport';
 
 export const routes = (
    
@@ -13,9 +14,9 @@ export const routes = (
 
         <Route exact path="/" element={<Home />} />
         <Route exact path="/transition-rate" element={<TransitionRateReport/>} />
-        <Route exact path="/Percentage-of-elementary-schools-having-PTR-less-than-equal-to-30" element={<TeacherAndSchResourcesReport/>} />
-        <Route exact path="/Percentage-Schools-with-Teachers-trained-for-teaching-CWSN" element={<StudentsPerformanceReport />} />
-        <Route exact path="/Percentange-of-Schools-having-adequate-Functional-Girls-Toilets" element={<SchoolInfraStructureReport />} />
+        <Route exact path="/teacher-and-school-resources" element={<TeacherAndSchResourcesReport/>} />
+        <Route exact path="/teachers-trained-for-teaching-CWSN" element={<TeacherTrainedCwsnReport />} />
+        <Route exact path="/school-infrastructure" element={<SchoolInfraStructureReport />} />
         <Route exact path="/enrollment-retention" element={<EnrollmentAndRetentionReport />} />
         <Route exact path="/screen-reader-access" element={<ScreenReader/>} />
 
